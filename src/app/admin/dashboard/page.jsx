9ccc6005df9d3cea723c71e71363553524ca19a5
prohostix <prohostix@github.com>
2026-04-08 09:49:25 +0530

    Fix Internal React error in Admin Dashboard by converting page to Server Component

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -1,4 +1,3 @@
-"use client";
 import Dashboard from '../../../views/Admin/Dashboard';
 import ProtectedRoute from '../../../components/ProtectedRoute';
 export default function Page() { return <ProtectedRoute><Dashboard /></ProtectedRoute>; }
